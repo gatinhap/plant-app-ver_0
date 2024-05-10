f3a72aa351f17e99426e9f60b0d900b01133d9ce
julia-piorek <julia.piorek@webproproductions.com>
2024-05-10 11:37:01 +0200

    query client and browser router providers added to custom render method

--- tests/test-utlis.tsx
+++ tests/test-utlis.tsx
@@ -2,15 +2,26 @@ import { ReactElement, ReactNode } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../src/theme/theme";
 import { render, RenderOptions } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 export const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">
 ) => {
-  const ThemeProviderWrapper = ({ children }: { children: ReactNode }) => {
-    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const queryClient = new QueryClient();
+
+  const AllTheProviders = ({ children }: { children?: ReactNode }) => {
+    return (
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    );
   };
-  return render(ui, { wrapper: ThemeProviderWrapper, ...options });
+  return render(ui, { wrapper: AllTheProviders, ...options });
 };
 
 export { customRender as render };
