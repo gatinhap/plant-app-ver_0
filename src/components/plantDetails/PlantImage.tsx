52aae50f1783e6847f40a030564fea428fa07438
julia-piorek <julia.piorek@webproproductions.com>
2024-12-02 16:33:54 +0100

    plant-details-eslint-fixes-applied

--- src/components/plantDetails/PlantImage.tsx
+++ src/components/plantDetails/PlantImage.tsx
@@ -2,12 +2,10 @@ import placeholder from '../../assets/placeholder.jpg';
 import { PlantImageContainerStyled } from './PlantDetail.styles.ts';
 import { PlantImageProps } from './PlantDetail.types.ts';
 
-function PlantImage({ isCustomImage = true, imgSrc }: PlantImageProps) {
-  return (
-    <PlantImageContainerStyled>
-      <img alt="plant image" src={isCustomImage ? imgSrc : placeholder} />
-    </PlantImageContainerStyled>
-  );
-}
+const PlantImage = ({ isCustomImage = true, imgSrc }: PlantImageProps) => (
+  <PlantImageContainerStyled>
+    <img alt="plant" src={isCustomImage ? imgSrc : placeholder} />
+  </PlantImageContainerStyled>
+);
 
 export default PlantImage;
