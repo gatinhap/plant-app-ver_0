c4b7803c675a3e95a356750100f0efe0a7952ab4
julia-piorek <julia.piorek@webproproductions.com>
2024-12-09 15:38:06 +0100

    eslint refactor and fixes applied

--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -1,21 +1,27 @@
 import PageComponent from '../../components/pageComponent/PageComponent.tsx';
 import CallToActionAsLink from '../../components/callToActionButton/CallToActionAsLink.tsx';
 import Text from '../../components/text/Text.tsx';
+import { ParagraphVariantEnum } from '../../components/text/Text.types.tsx';
+import StaticText from '../pages.constants.ts';
+import AppPaths from '../../config/appPaths.ts';
 
 const WelcomePage = () => (
-    <PageComponent>
-      <h3>hej, tu PlantMomma</h3>
+  <PageComponent>
+    <h3>{StaticText.WELCOME_PAGE_HEADING}</h3>
 
-      <Text variant="large">
-        Zaloguj się lub zarejestruj, aby korzystać z aplikacji!
-      </Text>
+    <Text variant={ParagraphVariantEnum.large}>
+      {StaticText.WELCOME_PAGE.WELCOME_TEXT}
+    </Text>
 
-      <CallToActionAsLink linkTo="/login">Zaloguj</CallToActionAsLink>
+    <CallToActionAsLink linkTo={AppPaths.Public.Login}>
+      {' '}
+      {StaticText.WELCOME_PAGE.LOGIN_LINK}
+    </CallToActionAsLink>
 
-      <CallToActionAsLink linkTo="/registration">
-        Zarejestruj
-      </CallToActionAsLink>
-    </PageComponent>
-  )
+    <CallToActionAsLink linkTo={AppPaths.Public.Register}>
+      {StaticText.WELCOME_PAGE.REGISTRATION_LINK}
+    </CallToActionAsLink>
+  </PageComponent>
+);
 
 export default WelcomePage;
