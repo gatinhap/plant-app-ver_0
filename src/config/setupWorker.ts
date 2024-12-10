76afb7f9cd1a8371e17ebb4a0e3e3e9568bf3bac
julia-piorek <julia.piorek@webproproductions.com>
2024-12-10 17:10:47 +0100

    mockServiceWorker.js added to public folder

--- src/config/setupWorker.ts
+++ src/config/setupWorker.ts
@@ -1,6 +1,4 @@
 import { SetupWorker, setupWorker } from 'msw/browser';
 import { handlers } from '../../tests/requestHandlers.ts';
 
-const worker: SetupWorker = setupWorker(...handlers);
-
-export default worker;
+export const worker: SetupWorker = setupWorker(...handlers);
