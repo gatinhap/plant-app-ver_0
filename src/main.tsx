85c0dfc3471d33d3f863c84f33c635959cadf397
julia-piorek <julia.piorek@webproproductions.com>
2025-01-04 17:20:56 +0100

    feat: radix library implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import '@radix-ui/themes/styles.css';
 
 async function enableMocking() {
   if (
